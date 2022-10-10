7cda9645a3fbcbf2542a3ac4e135ec28cae754f4
karuna-chaudhary <karunachaudhary186@gmail.com>
2022-10-10 19:20:45 +0530

    doubly ll modify

--- LinkedListProgram.java/DoublyLL.java
+++ LinkedListProgram.java/DoublyLL.java
@@ -66,8 +66,8 @@ public class DoublyLL {
     public static void main(String[] args){
        DoublyLL dll=new DoublyLL();
         
+        dll.addFirst(5);
         dll.addFirst(4);
-        dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
 
